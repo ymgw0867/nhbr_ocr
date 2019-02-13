37889f180fcdc63c89cdcfc724633995fe3d709b
ymgw0867 <kyamagiwa@gmail.com>
2019-02-13 10:58:45 +0900

    発注書備考欄印字機能追加

--- NHBR_OCR/Pattern/frmPrnPtn.cs
+++ NHBR_OCR/Pattern/frmPrnPtn.cs
@@ -812,7 +812,8 @@ namespace NHBR_OCR.Pattern
                     }
 
                     // 備考
-                    //rtnArray[62, 2] = s.備考; 2018/10/02
+                    //rtnArray[62, 2] = s.備考; 2018/10/02　コメント化
+                    rtnArray[62, 78] = s.備考 + Environment.NewLine;    // 2019/02/13
                     
                     // 配列からシートセルに一括してデータをセットします
                     rng = oxlsSheet.Range[oxlsSheet.Cells[1, 1], oxlsSheet.Cells[oxlsSheet.UsedRange.Rows.Count, oxlsSheet.UsedRange.Columns.Count]];
